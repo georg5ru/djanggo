54dc96934b2699da65818f6944f92055c8b7a911
ra1nfall~ <georg555ru@gmail.com>
2025-12-09 12:22:56 +0300

    changed

--- catalog/views.py
+++ catalog/views.py
@@ -2,17 +2,27 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
+
 def home(request):
     """Главная страница со списком товаров"""
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'catalog/home.html', context)
 
+
 def product_detail(request, pk):
-    """Страница с подробной информацией о товаре"""
+    """Страница с подробной информацией о товаре + связанные товары"""
     product = get_object_or_404(Product, pk=pk)
-    context = {'product': product}
+
+    # Получаем до 3 других товаров из той же категории, исключая текущий
+    related_products = product.category.product_set.exclude(pk=product.pk)[:3]
+
+    context = {
+        'product': product,
+        'related_products': related_products,
+    }
     return render(request, 'catalog/product_detail.html', context)
+
 
 def contacts(request):
     """Страница контактов"""
